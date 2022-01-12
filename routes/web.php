9283b42016c5d5c67cf497a1bdc2e27af75e0af6
Aladdin Taijan <aladdin.taijan@gmail.com>
2022-01-12 17:57:32 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Admin\UserController ;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.login');
 });
 
 Route::get('/forgetPass', function () {
     return view('admin.password');
 })->name('forgetPass');
+
+Route::prefix('admin')->name('admin.')->group(function (){
+    Route::resource('/users',UserController::class);
+});
+
+
